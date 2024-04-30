99635be39647d1f1cc192d61aa84c924bb21ebcb
Anku07 <as19961107@gmail.com>
2024-04-30 21:25:10 +0530

    deleting and adding of transaction

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,27 @@
 import styles from "./Home.module.css";
-import { useAuthContext } from '../../hooks/useAuthContext'
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
 // components
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 export default function Home() {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
+  const { document, error } = useCollection(
+    "transaction",
+    ["uid", "==", user.uid], // Query parameter
+    ["createdAt", "desc"] // OrderBy parameter
+  );
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {document && <TransactionList transactions={document} />}
+      </div>
       <div className={styles.sidebar}>
-        <TransactionForm uid= {user.uid} />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
